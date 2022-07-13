22ad55c39be996e9a91e7a11a9df38fc8afafce2
Matt <60991158+matthewfee@users.noreply.github.com>
2022-07-13 11:26:05 +0200

    added fade in animation

--- components/TempDisplay.js
+++ components/TempDisplay.js
@@ -4,9 +4,11 @@ import WeatherInfo from './WeatherInfo';
 
 const TempDisplay = ({ weather, temperature, sunrise, sunset, isDaytime, timezone }) => (
   <div className="temperature-result-container flex container justify-center p-1 m-0 h-60 relative">
-    <div className="weather-icon w-[150px] mx-2 flex justify-center items-center relative">
-      <Icon iconID={weather?.icon} />
-    </div>
+    {weather && (
+      <div className="weather-icon w-[150px] mx-2 flex justify-center items-center relative">
+        <Icon iconID={weather?.icon} />
+      </div>
+    )}
 
     {weather && (
       <WeatherInfo
