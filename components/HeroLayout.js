5d3c41a7c68b02339563bc936d2e93741fe06fc5
Matt <60991158+matthewfee@users.noreply.github.com>
2022-07-14 09:34:05 +0200

    added smoother animations for color transitions

--- components/HeroLayout.js
+++ components/HeroLayout.js
@@ -1,18 +1,23 @@
-import { dayBackgroundImageString, nightBackgroundImageString } from '../constants/constants';
-
 const HeroLayout = ({ children, isDaytime }) => (
   <div
-    className="hero hero-background h-screen min-h-[110vh] w-screen min-w-fit"
+    className="hero hero-background h-screen min-h-[110vh] w-screen min-w-fit  "
     style={{
       backgroundSize: 'cover',
       backgroundPosition: 'center bottom',
-      backgroundImage: `${isDaytime ? dayBackgroundImageString : nightBackgroundImageString}`,
+      backgroundImage: `url(https://picsum.photos/2000/1400)`,
     }}
   >
-    <div className="hero-overlay" />
     <div
-      className="hero-content w-full h-1/2 text-center text-neutral-content
-      max-w-md flex flex-col justify-start rounded-md p-0 m-0"
+      className={`hero-overlay ${
+        isDaytime ? `opacity-50` : `opacity-75`
+      }  transition-all ease-in-out duration-[4000ms] `}
+      style={{
+        backgroundColor: `${isDaytime ? '#0788D9' : 'black'}`,
+      }}
+    />
+    <div
+      className="hero-content w-full text-center text-neutral-content
+      max-w-md flex flex-col justify-center rounded-md p-0 m-0"
     >
       {children}
     </div>
