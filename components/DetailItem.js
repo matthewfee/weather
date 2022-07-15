f6928e57eab34e6c242fc2fc402b9394df28e7bc
Matt <60991158+matthewfee@users.noreply.github.com>
2022-07-15 13:54:04 +0200

    added smaller icons for details

--- components/DetailItem.js
+++ components/DetailItem.js
@@ -1,5 +1,5 @@
 export const DetailItem = ({ children, styles }) => (
-  <li className={`text-sm text-center flex flex-col justify-center md:px-6 px-2  mx-0 ${styles}`}>
+  <li className={`text-sm text-center flex flex-col justify-center md:px-4 px-2  mx-0 ${styles}`}>
     {children}{' '}
   </li>
 );
