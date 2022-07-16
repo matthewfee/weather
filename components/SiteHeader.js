fc862ec839b6e175aa890163cc1ba34f4506db51
Matt <60991158+matthewfee@users.noreply.github.com>
2022-07-16 13:06:08 +0200

    added color to site header

--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -2,7 +2,7 @@ const SiteHeader = ({ weather }) => (
   <h2
     className={`absolute tracking-widest text-center w-fit rounded px-2 font-bold letter uppercase ${
       weather
-        ? 'opacity-95 top-[-40px] text-2xl bg-[#023469]/50'
+        ? 'opacity-95 top-[-40px] text-[#D4E6FC] text-2xl bg-[#023469]/50'
         : 'opacity-100 top-[-100px] text-3xl md:text-6xl text-white no-wrap'
     } text-white `}
   >
