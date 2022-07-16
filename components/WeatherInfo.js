73fad1945589810f6e5913f87e248a9f73924b1f
Matt <60991158+matthewfee@users.noreply.github.com>
2022-07-16 10:31:43 +0200

    increased breathing room for main sections

--- components/WeatherInfo.js
+++ components/WeatherInfo.js
@@ -18,7 +18,7 @@ const WeatherInfo = ({
   <WeatherSectionContainer weather={weather}>
     {weather && <DetailsSection weatherDetails={weatherDetails} />}
 
-    <div className="weather-container-main md:px-4 flex flex-row flex-wrap justify-evenly items-center h-fill bg-slate-600/25 w-full mx-0 shadow-sm shadow-black/25 rounded my-2 py-4">
+    <div className="weather-container-main md:px-4 flex flex-row flex-wrap justify-evenly items-center h-fill my-8 py-4 bg-slate-600/25 w-full mx-0 shadow-sm shadow-black/25 rounded ">
       {temperature && <TemperatureContainer temperature={temperature} />}
       {weather && (
         <div className="md:max-w-[250px] max-w-[160px] flex flex-col justify-between items-center text-center relative">
