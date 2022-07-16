805566067ba51ed850e5e0d5279e4677906cc9c1
Matt <60991158+matthewfee@users.noreply.github.com>
2022-07-16 12:21:24 +0200

    increased readaibility of main site header

--- components/SiteHeader.js
+++ components/SiteHeader.js
@@ -3,7 +3,7 @@ const SiteHeader = ({ weather }) => (
     className={`absolute tracking-widest text-center w-full font-bold letter  uppercase ${
       weather
         ? 'opacity-95 h-0 top-[-40px] text-2xl '
-        : 'opacity-100 top-[-100px] text-4xl md:text-6xl text-white'
+        : 'opacity-100 top-[-100px] text-4xl md:text-7xl text-white no-wrap'
     } text-white `}
   >
     Sky Search
