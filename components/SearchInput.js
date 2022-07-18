0909479ce068c3436d713473c7532f214750d45c
Matt <60991158+matthewfee@users.noreply.github.com>
2022-07-18 15:31:51 +0200

    darkened input placeholder text

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -3,7 +3,7 @@ const SearchInput = ({ weather, value, onChange, onKeyPress }) => (
     type="text"
     htmlFor="location-input"
     placeholder="Search your city..."
-    className={`location-input h-[3rem] max-w-[16rem] bg-[#238CE8] rounded-md backdrop-blur-2xl p-4 text-center text-2xl text-white duration-[2000ms] focus:outline-white/25 input-primary placeholder:text-white/90  ${
+    className={`location-input h-[3rem] max-w-[16rem] bg-[#238CE8] rounded-md backdrop-blur-2xl p-4 text-center text-2xl text-white duration-[2000ms] focus:outline-white/25 input-primary placeholder:text-white/75  ${
       weather ? `bg-opacity-20 border border-black/5` : `bg-opacity-50`
     }  `}
     value={value}
