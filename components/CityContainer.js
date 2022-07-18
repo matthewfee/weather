902ed9e9b5fbc29fe8c8a3f30f98def5163d4b79
Matt <60991158+matthewfee@users.noreply.github.com>
2022-07-18 16:11:27 +0200

    added function to capitalized names and states

--- components/CityContainer.js
+++ components/CityContainer.js
@@ -1,11 +1,32 @@
-const CityContainer = ({ location, isDaytime }) => (
-  <h2
-    className={`city-name md:text-2xl text-xl md:pt-0 text-center capitalize tracking-wide ${
-      isDaytime ? 'text-[#b8d7fc]' : 'text-white'
-    }  font-semibold `}
-  >
-    {location}
-  </h2>
-);
+const CityContainer = ({ location, isDaytime }) => {
+  const fixCityCapitalization = (cityName) => {
+    if (cityName.includes(',')) {
+      const arr = cityName.split(',');
+
+      const capitalizedArr = arr.map((item, index) => {
+        if (index !== 0) {
+          return item.toUpperCase();
+        }
+        return item;
+      });
+
+      const capitalizedName = capitalizedArr.join(',');
+
+      return capitalizedName;
+    }
+
+    return cityName;
+  };
+
+  return (
+    <h2
+      className={`city-name md:text-2xl text-xl md:pt-0 text-center capitalize tracking-wide ${
+        isDaytime ? 'text-[#b8d7fc]' : 'text-white'
+      }  font-semibold `}
+    >
+      {fixCityCapitalization(location)}
+    </h2>
+  );
+};
 
 export default CityContainer;
